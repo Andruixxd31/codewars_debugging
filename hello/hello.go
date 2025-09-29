1dd752a3c6a45295042174e808c95be70c808a7a
Andres <andruix93@gmail.com>
2025-09-29 13:49:54 -0600

    feat: hello problem solved

---
+++ hello/hello.go
@@ -0,0 +1,72 @@
+package kata
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Dinglemouse struct {
+	name  string
+	age   int
+	sex   rune
+	order []string
+	keys  map[string]bool
+}
+
+func NewDinglemouse() *Dinglemouse {
+	return &Dinglemouse{
+		keys: make(map[string]bool),
+	}
+}
+
+func (d *Dinglemouse) SetAge(age int) *Dinglemouse {
+	addIfNew(d.keys, &d.order, "age")
+
+	d.age = age
+	return d
+}
+
+func (d *Dinglemouse) SetSex(sex rune) *Dinglemouse {
+	addIfNew(d.keys, &d.order, "sex")
+
+	d.sex = sex
+	return d
+}
+
+func (d *Dinglemouse) SetName(name string) *Dinglemouse {
+	addIfNew(d.keys, &d.order, "name")
+
+	d.name = name
+	return d
+}
+
+func addIfNew(keys map[string]bool, order *[]string, key string) {
+	if _, ok := keys[key]; !ok {
+		keys[key] = true
+		*order = append(*order, key)
+	}
+}
+
+func (d *Dinglemouse) Hello() string {
+	sexText := "male"
+
+	if d.sex == 'F' {
+		sexText = "female"
+	}
+
+	var b strings.Builder
+	b.WriteString("Hello.")
+
+	for _, v := range d.order {
+		switch v {
+		case "name":
+			b.WriteString(fmt.Sprintf(" My name is %s.", d.name))
+		case "age":
+			b.WriteString(fmt.Sprintf(" I am %d.", d.age))
+		case "sex":
+			b.WriteString(fmt.Sprintf(" I am %s.", sexText))
+		}
+	}
+
+	return b.String()
+}
